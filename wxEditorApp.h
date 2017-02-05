f32ceedca5e31585434e6cb18c415e57cd119ca0
Guillermo Garrido <G6In3D@users.noreply.github.com>
2017-02-04 22:00:02 -0500

    wxEditorApp.h

---
+++ wxEditorApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      wxEditorApp.h
+ * Purpose:   Defines Application Class
+ * Author:    Guillermo Garrido (ggarr018@fiu.edu)
+ * Created:   2017-01-31
+ * Copyright: Guillermo Garrido ()
+ * License:
+ **************************************************************/
+
+#ifndef WXEDITORAPP_H
+#define WXEDITORAPP_H
+
+#include <wx/app.h>
+
+class wxEditorApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // WXEDITORAPP_H
