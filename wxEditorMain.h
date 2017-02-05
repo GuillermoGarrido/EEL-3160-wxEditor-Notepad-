16f2dff1db7665f0dc3802f3fd55b29a4565945c
Guillermo Garrido <G6In3D@users.noreply.github.com>
2017-02-04 22:00:50 -0500

    wxEditorMain.h

---
+++ wxEditorMain.h
@@ -0,0 +1,37 @@
+/***************************************************************
+ * Name:      wxEditorMain.h
+ * Purpose:   Defines Application Frame
+ * Author:    Guillermo Garrido (ggarr018@fiu.edu)
+ * Created:   2017-01-31
+ * Copyright: Guillermo Garrido ()
+ * License:
+ **************************************************************/
+
+#ifndef WXEDITORMAIN_H
+#define WXEDITORMAIN_H
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include "wxEditorApp.h"
+
+class wxEditorFrame: public wxFrame
+{
+    public:
+        wxEditorFrame(wxFrame *frame, const wxString& title);
+        ~wxEditorFrame();
+    private:
+        enum
+        {
+            idMenuQuit = 1000,
+            idMenuAbout
+        };
+        void OnClose(wxCloseEvent& event);
+        void OnQuit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
+        DECLARE_EVENT_TABLE()
+};
+
+
+#endif // WXEDITORMAIN_H
